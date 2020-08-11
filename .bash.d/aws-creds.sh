fb4c7c6a07d6657830aab718121ecbeb823f1a2b
KGoldsworthy <KieranG@kgol.xyz>
2020-08-11 18:45:47 +1000

    nest aws-creds under .aws folder

--- .bash.d/aws-creds.sh
+++ .bash.d/aws-creds.sh
@@ -8,33 +8,37 @@
 aws-creds() {
 
     case $1 in
+        i | init) # Create folders
+            mkdir $HOME/.aws/cached-creds
+            mkdir $HOME/.aws/config-sets
+            ;;
         l | load)  # Load an AWS profile
 
             # awk 1 = The seperator is everything between ']' and '['. Therefore, only what is inside the square brackets are piped on.
             #    This will therefore return a list of 'profile <profile>'. (except the first line, which would be 'default').
             # awk 2 = If the line is greater than 1 (i.e. not the first line), then print the second item.
             #    This therefore prints all and only the roles that can be assumed
-            PROFILES=$(awk -F"\\\]|\\\[" '/^\[/{print $2}' ~/.aws/config | awk '{if(NR>1)print $2}')
+            PROFILES=$(awk -F"\\\]|\\\[" '/^\[/{print $2}' $HOME/.aws/config | awk '{if(NR>1)print $2}')
 
             # This provides a multiple choice method of selecting a profile.
             #    The role will then be assumed and the resulting creds will be exported to the environment varibles
             select PROFILE in $PROFILES; do
 
-                if [ -f "$HOME/.aws-creds/profiles/$PROFILE" ]  # if auto-rotated creds exist, load; else create
+                if [ -f "$HOME/.aws/cached-creds/$PROFILE" ]  # if auto-rotated creds exist, load; else create
                 then
-                    timeSinceLastMod=$(expr $(date +%s) - $(stat -c %Y "$HOME/.aws-creds/profiles/$PROFILE"))
+                    timeSinceLastMod=$(expr $(date +%s) - $(stat -c %Y "$HOME/.aws/cached-creds/$PROFILE"))
                     echo $timeSinceLastMod
                     if [ $timeSinceLastMod -gt 3600 ]
                     then
                         res=`aws sts assume-role --role-arn $(aws configure get $PROFILE.role_arn) --role-session-name my_profile_session --profile $PROFILE`
-                        echo "$res" >| ~/.aws-creds/profiles/$PROFILE
+                        echo "$res" >| $HOME/.aws/cached-creds/$PROFILE
                         export AWS_ACCESS_KEY_ID=`echo $res | jq -r '.Credentials.AccessKeyId'`
                         export AWS_SECRET_ACCESS_KEY=`echo $res | jq -r '.Credentials.SecretAccessKey'`
                         export AWS_SESSION_TOKEN=`echo $res | jq -r '.Credentials.SessionToken'`
                         export AWS_REGION=`aws configure get region --profile $PROFILE`
 
                     else
-                        res="$(cat ~/.aws-creds/profiles/$PROFILE)"
+                        res="$(cat $HOME/.aws/cached-creds/$PROFILE)"
                         export AWS_ACCESS_KEY_ID=`echo $res | jq -r '.Credentials.AccessKeyId'`
                         export AWS_SECRET_ACCESS_KEY=`echo $res | jq -r '.Credentials.SecretAccessKey'`
                         export AWS_SESSION_TOKEN=`echo $res | jq -r '.Credentials.SessionToken'`
@@ -43,13 +47,13 @@ aws-creds() {
 
                 else
                     res=`aws sts assume-role --role-arn $(aws configure get $PROFILE.role_arn) --role-session-name my_profile_session --profile $PROFILE`
-                    echo "$res" >| ~/.aws-creds/profiles/$PROFILE
+                    echo "$res" >| $HOME/.aws/cached-creds/$PROFILE
                     export AWS_ACCESS_KEY_ID=`echo $res | jq -r '.Credentials.AccessKeyId'`
                     export AWS_SECRET_ACCESS_KEY=`echo $res | jq -r '.Credentials.SecretAccessKey'`
                     export AWS_SESSION_TOKEN=`echo $res | jq -r '.Credentials.SessionToken'`
                     export AWS_REGION=`aws configure get region --profile $PROFILE`
 
-                    #aws-creds-delayed-delete $HOME/.aws-creds/profiles/$PROFILE &
+                    #aws-creds-delayed-delete $HOME/.aws/cached-creds/$PROFILE &
                 fi
 
                 export AWS_CREDS_PROFILE=$PROFILE
@@ -57,10 +61,10 @@ aws-creds() {
                 break
             done
             ;;
-        r | reload)  # Reload auto-rotated creds for most recent profile
+        r | reload)  # Reload creds for most recent profile
             PROFILE=$AWS_CREDS_PROFILE
             res=`aws sts assume-role --role-arn $(aws configure get $PROFILE.role_arn) --role-session-name my_profile_session --profile $PROFILE`
-            echo "$res" >| ~/.aws-creds/profiles/$PROFILE
+            echo "$res" >| $HOME/.aws/cached-creds/$PROFILE
             export AWS_ACCESS_KEY_ID=`echo $res | jq -r '.Credentials.AccessKeyId'`
             export AWS_SECRET_ACCESS_KEY=`echo $res | jq -r '.Credentials.SecretAccessKey'`
             export AWS_SESSION_TOKEN=`echo $res | jq -r '.Credentials.SessionToken'`
@@ -93,7 +97,7 @@ aws-creds() {
             echo "Note: The details above are taken from your environment variables; if you have manally exported credentials then the api keys might not match the profile."
             ;;
         c | clear)  # Delete all current profiles
-            rm $HOME/.aws-creds/profiles/*
+            rm $HOME/.aws/cached-creds/*
             unset AWS_ACCESS_KEY_ID
             unset AWS_SECRET_ACCESS_KEY
             unset AWS_SESSION_TOKEN
@@ -102,7 +106,7 @@ aws-creds() {
             ;;
         * )  # Else print help
             echo ""
-            echo "  aws-creds [ load | reload | assume | export | show ]"
+            echo "  aws-creds [ init | load | reload | assume | export | show ]"
             echo ""
             echo "  aws-creds is a tool for quickly switching between AWS cli profiles."
             echo "  It exports the credentials into environment variables for easy use with most AWS tools."
@@ -110,6 +114,9 @@ aws-creds() {
             echo "  The rotation is done every 30 min to avoid needing to enter an MFA token again (temp creds making temp creds)."
             echo "  The credentials are also saved to disk temporarily which enables quick switching between profiles without needing to reenter an MFA token"
             echo "  Note that some intrusion detection tools might look for temp creds making temp creds and consider this a breach."
+            echo ""
+            echo "    i | init )"
+            echo "      Create folders."
             echo ""
             echo "    l | load )"
             echo "      Load a profile."
