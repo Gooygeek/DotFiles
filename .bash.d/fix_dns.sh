2e8694e08272e7c33b85f13495876e63d507005a
Kieran Goldsworthy <kierang@kgol.xyz>
2021-09-13 12:26:51 +1000

    Update fix_dns.sh

--- .bash.d/fix_dns.sh
+++ .bash.d/fix_dns.sh
@@ -2,6 +2,7 @@
 # Note that this is run every time a session is started
 # This is particularly important of wsl
 
-cat > /etc/resolv.conf <<EOF
+cat > /etc/resolv.conf.tmp <<EOF
 nameserver 1.1.1.1
 EOF
+sudo mv /etc/resolv.conf.tmp /etc/resolv.conf
