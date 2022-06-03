b68a7b4b54ee6509b48f7099b0fb7f1e5c1f5b63
Kieran Goldsworthy <KieranG@kgol.xyz>
2022-06-03 13:58:36 +1000

    Add additional highlighters to zsh

--- .config/zsh/essentials.zsh
+++ .config/zsh/essentials.zsh
@@ -51,3 +51,5 @@ fi
 
 # zsh-syntax-highlighting
 source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets cursor)
+ZSH_HIGHLIGHT_STYLES[cursor]='bg=blue'
