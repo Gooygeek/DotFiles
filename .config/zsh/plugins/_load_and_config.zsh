b41bd42da0fbca41427a253aa4ec1db232f64adf
Kieran Goldsworthy <KieranG@kgol.xyz>
2022-06-03 13:58:18 +1000

    Set limit to zsh autosuggest length

--- .config/zsh/plugins/_load_and_config.zsh
+++ .config/zsh/plugins/_load_and_config.zsh
@@ -2,6 +2,7 @@
 # https://github.com/zsh-users/zsh-autosuggestions
 . ~/.config/zsh/plugins/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=50 # Don't suggest really long strings
 
 # zsh-syntax-highlighting
 # Needs to be sourced after everything else. Thus it's been pushed to the essentials file.
