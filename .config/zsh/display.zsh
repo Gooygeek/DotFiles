fabb731e4561b2ba0cee7e93e6f9f089c04569c6
Kieran Goldsworthy <kierang@kgol.xyz>
2022-06-07 01:15:44 +1000

    zshrc: set DISPLAY for WSL2

---
+++ .config/zsh/display.zsh
@@ -0,0 +1,6 @@
+
+# Set DISPLAY if running in WSL (WSL2 need to use hosts IP)
+# X server needds to be running on the host. mobaxterm has this.
+if cat /proc/version | grep "microsoft" 2>&1 1>/dev/null ; then
+  export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+fi
