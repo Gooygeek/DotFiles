6e6cf07432c88520ee1204e11948fb9788521bf7
Kieran Goldsworthy <kierang@kgol.xyz>
2022-06-18 00:43:27 +1000

    zshrc: add misc aliases

---
+++ .config/zsh/aliases/misc.zsh
@@ -0,0 +1,8 @@
+
+# Make a bell sound.
+# Great for the end of a long command.
+# e.g. some-long-command; bell
+alias bell='tput bel'
+
+# print the whole history. Only prints previous 16 by default
+alias history="history 0"
