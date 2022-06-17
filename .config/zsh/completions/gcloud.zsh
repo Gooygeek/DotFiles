a139e1e4570d44ca0ac24cd6bbf71f8c440e3d4e
Kieran Goldsworthy <kierang@kgol.xyz>
2022-06-18 01:30:33 +1000

    zshrc: better handling of gcloud completion (and work when not installed via homebrew)

--- .config/zsh/completions/gcloud.zsh
+++ .config/zsh/completions/gcloud.zsh
@@ -1,4 +1,9 @@
-if [ "$machine" = "Mac" ]; then
-    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-    source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+if command -v gcloud >/dev/null 2>&1; then
+    if [ "$machine" = "Mac" ]; then
+        source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+        source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    elif [ "$machine" = "Linux" ]; then
+        source "$(which gcloud | sed 's/\/gcloud$//' )/../completion.zsh.inc"
+        source "$(which gcloud | sed 's/\/gcloud$//' )/../path.zsh.inc"
+    fi
 fi
