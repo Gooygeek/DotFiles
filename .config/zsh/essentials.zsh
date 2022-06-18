d717ecbb6164fb79a4c2f6164c48a174a6b89674
Kieran Goldsworthy <kierang@kgol.xyz>
2022-06-19 00:51:50 +1000

    zshrc: add list of commands not to save to the history file

--- .config/zsh/essentials.zsh
+++ .config/zsh/essentials.zsh
@@ -55,12 +55,17 @@ done
 . ~/.config/zsh/plugins/_load_and_config.zsh
 
 
-## Other
+## History
 
 # Set file to save history
 export HISTFILE=~/.zsh_history
 export HISTSIZE=2000 # The maximum number of events stored in the internal history list.
 export SAVEHIST=1000 # The maximum number of history events to save in the history file.
+# Command patterns to NOT save to the history (only stops it being saved, does not affect interactive history)
+export HISTORY_IGNORE="(history|clear|exit|bg|fg|ls|ls *|lls| lls *| *)"
+
+
+## Other
 
 # Load super useful file renamer
 autoload zmv
