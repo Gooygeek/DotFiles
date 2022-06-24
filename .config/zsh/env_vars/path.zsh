49336a96cef0898eed0d9038a4513cfacf69c287
Kieran Goldsworthy <kierang@kgol.xyz>
2022-06-24 23:18:44 +1000

    zshrc: add more dirs to PATH

--- .config/zsh/env_vars/path.zsh
+++ .config/zsh/env_vars/path.zsh
@@ -4,3 +4,9 @@ PATH="$PATH:$HOME/.local/bin"
 if [[ -d /opt/homebrew/ ]]; then
     PATH="$PATH:/opt/homebrew/bin/"
 fi
+
+PATH="$PATH:$HOME/.fzf/bin"
+
+PATH="$PATH:/usr/local/go/bin"
+
+PATH="$PATH:$HOME/.tfenv/bin"
