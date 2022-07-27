5b2976aabf959a5c4f52b9d2c084f0ea6a04ec1c
Kieran Goldsworthy <kierang@kgol.xyz>
2022-07-27 13:14:34 +1000

    Fix gpg functions

--- .config/zsh/functions/gpg.sh
+++ .config/zsh/functions/gpg.sh
@@ -2,7 +2,7 @@
 # Unlock the gpg key used by the current git config to add it to the gpg-agent password cache
 unlock-current-git-gpg-key() {
     KeyId=$(git config --get user.signingKey)
-    KeyGrip=$(gpg --with-keygrip --list-secret-keys ${KeyId} | tail -2 | awk '{print $3}')
+    KeyGrip=$(gpg --with-keygrip --list-secret-keys ${KeyId} | head -3 | tail -1 | awk '{print $3}')
     KeyEmail=$(gpg --with-keygrip --list-secret-keys ${KeyId} | grep -o "<.*@.*>")
     # KeyCacheStatus=$(gpg-connect-agent "keyinfo ${KeyGrip}" /bye | awk '{print $7}') # "-" = not cached, "1" = cached
     # echo "Email: $KeyEmail\\nKey ID: $KeyId\\nKey Grip: $KeyGrip\\nCache Status: $KeyCacheStatus"
@@ -12,4 +12,8 @@ unlock-current-git-gpg-key() {
 # Reload config and clear password cache
 reload-gpg-agent() {
     echo "RELOADAGENT" | gpg-connect-agent > /dev/null
+}
+
+kill-gpg-agent() {
+    pkill -9 gpg-agent ; gpg-agent --daemon >/dev/null
 }
