d41129ca4ec4df5e736bb6663a6a42834ad641a5
Kieran Goldsworthy <kierang@kgol.xyz>
2022-08-04 00:22:05 +1000

    zshrc: universal format for sourcing gcloud completions

--- .config/zsh/completions/gcloud.zsh
+++ .config/zsh/completions/gcloud.zsh
@@ -1,9 +1,8 @@
 if command -v gcloud >/dev/null 2>&1; then
-    if [ "$machine" = "Mac" ]; then
-        source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-        source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-    elif [ "$machine" = "Linux" ]; then
-        source "$(which gcloud | sed 's/\/gcloud$//' )/../completion.zsh.inc"
-        source "$(which gcloud | sed 's/\/gcloud$//' )/../path.zsh.inc"
-    fi
+    # `gcloud info` method has fewer dependancies, but is slow
+    # source "$(gcloud info --format='value(installation.sdk_root)')/path.zsh.inc"
+    # source "$(gcloud info --format='value(installation.sdk_root)')/completion.zsh.inc"
+
+    source "$(\fd -L "completion.zsh.inc" $(readlink -f $(which gcloud) | sed 's/\/gcloud$//' )/..)"
+    source "$(\fd -L "path.zsh.inc" $(readlink -f $(which gcloud) | sed 's/\/gcloud$//' )/..)"
 fi
