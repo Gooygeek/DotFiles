444a02a099375b21c28b6558fee60d07147b65a8
Kieran Goldsworthy <kierang@kgol.xyz>
2022-11-20 14:39:46 +1100

    zshrc: check route.exe is installed before using it for setting the DISPLAY var

--- .config/zsh/display.zsh
+++ .config/zsh/display.zsh
@@ -2,5 +2,7 @@
 # Set DISPLAY if running in WSL (WSL2 need to use hosts IP)
 # X server needds to be running on the host. mobaxterm has this.
 if [[ -f /proc/version ]] && cat /proc/version | grep "microsoft" 2>&1 1>/dev/null ; then
-  export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+  if command -v route.exe >/dev/null 2>&1; then
+    export DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+  fi
 fi
