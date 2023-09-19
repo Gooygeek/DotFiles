187291bd9aae6eaf1ac651201ffcb5362d164912
Kieran Goldsworthy <kierang@kgol.xyz>
2023-09-19 12:00:04 +1000

    Fix lg alias re: renamed gpg unlock command

--- .config/zsh/aliases/git.zsh
+++ .config/zsh/aliases/git.zsh
@@ -3,6 +3,6 @@ alias g='git'
 # alias lg='lazygit'
 
 # Unlock gpg key before opening lazygit
-alias lg='unlock-current-git-gpg-key; lazygit'
+alias lg='gpg-unlock-current-git-key; lazygit'
 
 alias gishe='gite;sshe'
