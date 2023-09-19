75da3b00b5b6565877fb6de1c6258ef81dd9babc
Kieran Goldsworthy <kierang@kgol.xyz>
2023-09-19 12:00:47 +1000

    Fix fzf tab completion 'type' headings

--- .config/zsh/completions/config.zsh
+++ .config/zsh/completions/config.zsh
@@ -5,6 +5,6 @@ zstyle ':completion:*' menu select # Use menu selection
 zstyle ':completion:*' select-prompt "--vvvvv--" # Used to indicate when there are options that don't fit on the screen
 zstyle ':completion::complete:*' use-cache 1 # Enable completion cache
 zstyle ':completion::complete:*' cache-path ~/.config/zsh/completion.cache
-zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f' # Print descriptions for 'types'
+zstyle ':completion:*:descriptions' format '%d' # Print descriptions for 'types'
 zstyle ':completion:*' group-name '' # Group 'types'
 zstyle ':completion:*' squeeze-slashes true # convert `//`` to `/` instead of the default `/*/` as it's more cross compatible
