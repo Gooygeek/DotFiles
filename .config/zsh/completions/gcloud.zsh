69197b5edbb8366084d598720b805cee9aa0a1c4
Kieran Goldsworthy <kierang@kgol.xyz>
2024-02-09 23:54:21 +1100

    Refactor gcloud completion load = faster startup

--- .config/zsh/completions/gcloud.zsh
+++ .config/zsh/completions/gcloud.zsh
@@ -3,6 +3,6 @@ if command -v gcloud >/dev/null 2>&1; then
     # source "$(gcloud info --format='value(installation.sdk_root)')/path.zsh.inc"
     # source "$(gcloud info --format='value(installation.sdk_root)')/completion.zsh.inc"
 
-    source "$(\fd -L "completion.zsh.inc" $(readlink -f $(which gcloud) | sed 's/\/gcloud$//' )/..)"
-    source "$(\fd -L "path.zsh.inc" $(readlink -f $(which gcloud) | sed 's/\/gcloud$//' )/..)"
+    source "$(dirname $(readlink -f $(which gcloud)))/../completion.zsh.inc"
+    source "$(dirname $(readlink -f $(which gcloud)))/../path.zsh.inc"
 fi
