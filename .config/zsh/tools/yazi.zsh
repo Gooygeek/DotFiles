489a4b1ea791834b0ec722c5a191fafccb9300d4
Kieran Goldsworthy <kierang@kgol.xyz>
2024-06-28 01:10:24 +1000

    zsh: add wrapper script for yazi
    This function will cd to the directory that yazi was in when quiting

---
+++ .config/zsh/tools/yazi.zsh
@@ -0,0 +1,10 @@
+# Shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
